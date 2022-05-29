bf8b2c2c408b54f335584e611c39c7c71c457b2d
Jamie Fer <67526745+Lolicchi@users.noreply.github.com>
2022-05-29 11:36:59 +0530

    Update bot.ts

--- bot.ts
+++ bot.ts
@@ -5,11 +5,11 @@ import { IsekoWSC } from './mod.ts'
 // setTimeout(Deno.exit, 10 * 60 * 1000)
 
 const client = new IsekoWSC({
-  token: 'OTU3MTYyODA0NjE4NjAwNTE5.GqM9Ui.07qW2OdPy9YAO6OEr9WEo3YEx1wyIofs-JOPvY',
+  token: 'OTgwMzUxMjI4MDgzMDQ4NDk4.GnqpFy.JKssZomm1v_lsR5WvYwoUPCNek1-f3QzqZTtGg',
   onReady: () => {
     // client.close()
     client.setPresence({
-      activities: [{ name: 'Deno Dev 1.22.1', type: 2 }],
+      activities: [{ name: 'Deno Dev 1.22.1', type: 3 }],
       status: 'online'
     })
     client.joinVoice('914073971815776286', '914073971815776290')
